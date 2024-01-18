820913484d1b927585a10a5dbfe9c3ca6689967a
Sergey Tsybenko <cybenko.sergey@gmail.com>
2024-01-18 09:02:35 -0700

    Add loading animation

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,1 +1,20 @@
-console.log('Hello World!')
+function setLastModified(){
+    const elem = document.querySelector('#last-modified')
+    elem.textContent = document.lastModified.split(' ')[0]
+}
+
+function setCurYear(){
+    const elem = document.querySelector('#year')
+    elem.textContent = (new Date).getFullYear()
+}
+
+addEventListener("DOMContentLoaded", (event) => {
+    
+    setLastModified()
+    setCurYear()
+
+    setTimeout(() => {
+        const body = document.querySelector('body')
+        body.classList.remove('loading')
+    }, 2000)
+});
