91c845d322df9b660664aef39c355b012f7254b1
Sergey Tsybenko <cybenko.sergey@gmail.com>
2024-02-10 18:33:17 -0700

    Local Storage

---
+++ week03/scripts/local-storage.js
@@ -0,0 +1,55 @@
+const input = document.querySelector('#favchap')
+const button = document.querySelector('button')
+const list = document.querySelector('#list')
+
+let chaptersArray = getChapterList() || []
+
+
+function removeBookFromList(event) {
+	event.target.parentElement.remove()
+}
+
+button.addEventListener('click', () => {
+    if (input.value != '') { 
+      displayList(input.value)
+      chaptersArray.push(input.value) 
+      setChapterList()
+      input.value = ''
+      input.focus()
+    }
+  })
+
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter)
+})
+
+function displayList(item) {
+    let li = document.createElement('li')
+    let deletebutton = document.createElement('button')
+    li.textContent = item
+    deletebutton.textContent = '❌'
+    deletebutton.classList.add('delete')
+    li.append(deletebutton)
+    list.append(li)
+    deletebutton.addEventListener('click', function () {
+      list.removeChild(li)
+      deleteChapter(li.textContent)
+      input.focus()
+    })
+}
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray))
+}
+
+function getChapterList() {
+    console.log('Data', JSON.parse(localStorage.getItem('myFavBOMList')))
+    return JSON.parse(localStorage.getItem('myFavBOMList'))
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1)
+    chaptersArray = chaptersArray.filter(item => item !== chapter)
+    setChapterList()
+}
