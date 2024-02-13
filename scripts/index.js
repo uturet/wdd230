9c1d9088e2a0839e3e2466cd9fe9896d794daca4
Sergey Tsybenko <cybenko.sergey@gmail.com>
2024-02-13 08:29:03 -0700

    Add a page counter and a map

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,13 @@ burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('open')
     navigation.classList.toggle('open')
 })
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `0`;
+}
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
